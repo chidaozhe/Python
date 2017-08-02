8838142d0a518ad748a36b127a3d25b8b14d77f0
chidaozhe <liujigang0911@163.com>
2017-08-02 20:33:44 +0800

    Create ex21.py

---
+++ ex21.py
@@ -0,0 +1,35 @@
+def add(a,b):
+    print "ADDING %d + %d" % (a,b)
+    return a + b
+	
+def subtract(a,b):
+    print "SUBTRACTING %d - %d" % (a,b)
+    return a -b
+	
+def multiply(a,b):
+    print "MULTIPLYING %d * %d" % (a,b)
+    return a * b
+	
+def divide(a,b):
+    print "DIVIDING %d / %d" % (a,b)
+    return a / b
+	
+print "Let's do some math with just functions!"
+
+age = add(30,5)
+height = subtract(78,4)
+weight = multiply(90,2)
+iq = divide(100,2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+
+# A puzzle for the extra credit, type it in anyway.
+print "Here is a puzzle."
+
+a = divide(iq, 2)
+b = multiply(weight, a)
+c = subtract(height, b)
+d = add(age, c)
+
+print "That becomes: ", d, "Can you do it by hand?"
