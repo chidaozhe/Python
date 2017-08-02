1307b25be61059b062064324d3d8d449b7519030
chidaozhe <liujigang0911@163.com>
2017-08-02 20:25:15 +0800

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+age = raw_input("how are you")
+
+script, first, second, third = argv
+
+print "There script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your four variable is:", age
